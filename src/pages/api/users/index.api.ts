72086520b8acb376ad84c9accd9150fd18fdd85a
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-01-03 16:26:05 -0400

    set cookies

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../lib/prisma'
+import { setCookie } from 'nookies'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method != 'POST') {
@@ -8,11 +9,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const { name, username } = req.body
 
+    const userExists = await prisma.user.findUnique({
+        where: {
+            username
+        }
+    })
+
+    if (userExists) {
+        return res.status(400).json({
+            message: 'Username already taken.'
+        })
+    }
+
     const user = await prisma.user.create({
         data: {
             name,
             username
         }
+    })
+
+    setCookie({ res }, '@agendamento:userId', user.id, {
+        maxAge: 60 * 60 * 2, // 2 hour
+        path: '/'
     })
 
     return res.status(201).json(user)
